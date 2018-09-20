0bf8c63c92c520fba3197812745a125fb9b497bb
Karis Kim <ouniiq@hotmail.com>
2018-09-20 13:58:38 -0700

    completed wave 1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,13 @@
-TASKS = [
-  { task: "yawning", person: "Margot Lee Shetterly"},
-  { task: "eating", person: "Sandi Metz"},
-  { task: "sleeping", person: "Octavia E. Butler"}
-]
-
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
 
   def show
-    task_id = params[:id]
-    @task = TASKS[task_id]
+    task_id = params[:id].to_i
+    @tasks = Task.find_by(id: task_id)
+    if @tasks.nil?
+      head :not_found
+    end
   end
 end
